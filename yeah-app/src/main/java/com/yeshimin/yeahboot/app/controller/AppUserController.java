63fd05b4e4671865e41ccd9b9bfaa0787d1c481b
yeshimin <ysm@yeshimin.com>
2025-06-20 17:51:42 +0800

    clean code

--- yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
+++ yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("/user/appUser")
+@RequestMapping("/app/appUser")
 public class AppUserController extends CrudController<AppUserMapper, AppUserEntity, AppUserRepo> {
 
     @Autowired
