2b52ab6c581a44a2057f9dc18031b22194960cf0
yeshimin <ysm@yeshimin.com>
2025-06-20 10:28:45 +0800

    clean code

--- yeah-admin/src/test/java/com/yeshimin/yeahboot/JwtServiceTests.java
+++ yeah-admin/src/test/java/com/yeshimin/yeahboot/JwtServiceTests.java
@@ -3,7 +3,7 @@ package com.yeshimin.yeahboot;
 import com.alibaba.fastjson2.JSON;
 import com.yeshimin.yeahboot.admin.YeahAdminApplication;
 import com.yeshimin.yeahboot.auth.domain.vo.JwtPayloadVo;
-import com.yeshimin.yeahboot.upms.service.JwtService;
+import com.yeshimin.yeahboot.auth.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
--- yeah-auth/src/main/java/com/yeshimin/yeahboot/auth/service/JwtService.java
+++ yeah-auth/src/main/java/com/yeshimin/yeahboot/auth/service/JwtService.java
@@ -114,7 +114,6 @@ public class JwtService {
 //        return Objects.equals(audience, decodedJWT.getAudience().get(0));
 //    }
 
-
     /**
      * 解码Jwt
      */
