c4fdc1df3e3ef4d593b58e48d707f30d3669746a
yeshimin <ysm@yeshimin.com>
2025-06-24 22:07:38 +0800

    clean code

--- yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/BannerController.java
+++ yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/BannerController.java
@@ -1,25 +1,15 @@
 package com.yeshimin.yeahboot.merchant.controller;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.yeshimin.yeahboot.common.common.utils.WebContextUtils;
-import com.yeshimin.yeahboot.common.controller.base.BaseController;
-import com.yeshimin.yeahboot.common.domain.base.R;
-import com.yeshimin.yeahboot.merchant.controller.base.MchCrudController;
 import com.yeshimin.yeahboot.merchant.controller.base.ShopCrudController;
 import com.yeshimin.yeahboot.merchant.data.domain.entity.BannerEntity;
-import com.yeshimin.yeahboot.merchant.data.domain.entity.MerchantEntity;
 import com.yeshimin.yeahboot.merchant.data.mapper.BannerMapper;
-import com.yeshimin.yeahboot.merchant.data.mapper.MerchantMapper;
 import com.yeshimin.yeahboot.merchant.data.repository.BannerRepo;
-import com.yeshimin.yeahboot.merchant.data.repository.MerchantRepo;
 import com.yeshimin.yeahboot.merchant.service.BannerService;
-import com.yeshimin.yeahboot.merchant.service.MerchantService;
 import com.yeshimin.yeahboot.merchant.service.PermissionService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Banner相关
@@ -40,27 +30,4 @@ public class BannerController extends ShopCrudController<BannerMapper, BannerEnt
     }
 
     // ================================================================================
-
-//    /**
-//     * 创建
-//     */
-//    @PostMapping("/create")
-//    @Transactional(rollbackFor = Exception.class)
-//    public R<BannerEntity> create(@RequestBody BannerEntity e) {
-//        Long userId = WebContextUtils.getUserId();
-//        // check permission
-//        permissionService.checkShop(userId, e.getShopId());
-//        return R.ok(service.create(e));
-//    }
-//
-//    /**
-//     * 查询
-//     */
-//    @GetMapping("/query")
-//    public R<Page<BannerEntity>> query(Page<BannerEntity> page, BannerEntity query) {
-//        Long userId = WebContextUtils.getUserId();
-//        // check permission
-//        permissionService.checkShop(userId, query.getShopId());
-//        return R.ok(service.query(page, query));
-//    }
 }
--- yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/ShopController.java
+++ yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/ShopController.java
@@ -1,25 +1,12 @@
 package com.yeshimin.yeahboot.merchant.controller;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.yeshimin.yeahboot.common.common.utils.WebContextUtils;
-import com.yeshimin.yeahboot.common.controller.base.BaseController;
-import com.yeshimin.yeahboot.common.domain.base.R;
 import com.yeshimin.yeahboot.merchant.controller.base.MchCrudController;
-import com.yeshimin.yeahboot.merchant.controller.base.ShopCrudController;
-import com.yeshimin.yeahboot.merchant.data.domain.entity.BannerEntity;
 import com.yeshimin.yeahboot.merchant.data.domain.entity.ShopEntity;
-import com.yeshimin.yeahboot.merchant.data.mapper.BannerMapper;
 import com.yeshimin.yeahboot.merchant.data.mapper.ShopMapper;
-import com.yeshimin.yeahboot.merchant.data.repository.BannerRepo;
 import com.yeshimin.yeahboot.merchant.data.repository.ShopRepo;
-import com.yeshimin.yeahboot.merchant.service.BannerService;
-import com.yeshimin.yeahboot.merchant.service.PermissionService;
 import com.yeshimin.yeahboot.merchant.service.ShopService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,8 +18,6 @@ public class ShopController extends MchCrudController<ShopMapper, ShopEntity, Sh
 
     @Autowired
     private ShopService service;
-    @Autowired
-    private PermissionService permissionService;
 
     public ShopController(ShopRepo repo) {
         // 由于lombok方案无法实现构造方法中调用super，只能显式调用
@@ -40,26 +25,4 @@ public class ShopController extends MchCrudController<ShopMapper, ShopEntity, Sh
     }
 
     // ================================================================================
-
-//    /**
-//     * 查询
-//     */
-//    @GetMapping("/query")
-//    public R<Page<ShopEntity>> query(Page<ShopEntity> page, ShopEntity query) {
-//        Long userId = WebContextUtils.getUserId();
-//        // 权限控制
-//        query.setMchId(userId);
-//        return R.ok(service.query(page, query));
-//    }
-//
-//    /**
-//     * 详情
-//     */
-//    @GetMapping("/detail")
-//    public R<ShopEntity> detail(@RequestParam Long id) {
-//        Long userId = WebContextUtils.getUserId();
-//        // check permission
-//        ShopEntity entity = permissionService.getShop(userId, id);
-//        return R.ok(entity);
-//    }
 }
