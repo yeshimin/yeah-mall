4b106e7189377c5d5d865bf0bd20dea7056ccf70
yeshimin <ysm@yeshimin.com>
2025-07-15 21:27:21 +0800

    api: /app/order/query - 查询个人订单

--- yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppOrderController.java
+++ yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppOrderController.java
@@ -1,15 +1,15 @@
 package com.yeshimin.yeahboot.app.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yeshimin.yeahboot.app.domain.dto.OrderQueryDto;
 import com.yeshimin.yeahboot.app.domain.dto.OrderSubmitDto;
 import com.yeshimin.yeahboot.app.service.AppOrderService;
 import com.yeshimin.yeahboot.common.controller.base.BaseController;
 import com.yeshimin.yeahboot.common.domain.base.R;
+import com.yeshimin.yeahboot.data.domain.entity.OrderEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 订单表
@@ -29,5 +29,14 @@ public class AppOrderController extends BaseController {
         Long userId = super.getUserId();
         appOrderService.submit(userId, dto);
         return R.ok();
+    }
+
+    /**
+     * 查询个人订单
+     */
+    @GetMapping("/query")
+    public R<Page<OrderEntity>> query(Page<OrderEntity> page, @Validated OrderQueryDto dto) {
+        Long userId = super.getUserId();
+        return R.ok(appOrderService.query(userId, page, dto));
     }
 }
---
+++ yeah-app/src/main/java/com/yeshimin/yeahboot/app/domain/dto/OrderQueryDto.java
@@ -0,0 +1,20 @@
+package com.yeshimin.yeahboot.app.domain.dto;
+
+import com.yeshimin.yeahboot.app.common.enums.OrderStatusEnum;
+import com.yeshimin.yeahboot.common.common.config.mybatis.Query;
+import com.yeshimin.yeahboot.common.common.validation.EnumValue;
+import com.yeshimin.yeahboot.common.domain.base.BaseQueryDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Query(custom = true)
+public class OrderQueryDto extends BaseQueryDto {
+
+    /**
+     * 订单状态：订单表
+     */
+    @EnumValue(enumClass = OrderStatusEnum.class)
+    private String status;
+}
