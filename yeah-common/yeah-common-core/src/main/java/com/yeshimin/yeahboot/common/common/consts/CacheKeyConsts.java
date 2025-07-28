74aca58fd7ebfd52d243334a16ca161c51b8ff2c
yeshimin <ysm@yeshimin.com>
2025-07-28 20:57:38 +0800

    clean code

--- yeah-common/yeah-common-core/src/main/java/com/yeshimin/yeahboot/common/common/consts/CacheKeyConsts.java
+++ yeah-common/yeah-common-core/src/main/java/com/yeshimin/yeahboot/common/common/consts/CacheKeyConsts.java
@@ -5,11 +5,6 @@ package com.yeshimin.yeahboot.common.common.consts;
  */
 public class CacheKeyConsts {
 
-    /**
-     * 用户token缓存key
-     * 带终端的，subject : user : terminal
-     */
-    public static final String USER_TOKEN_TERM = "sub:%s:user:%s:token:term:%s";
     // subject下所有终端信息
     public static final String USER_SUBJECT_TERMINAL_INFO = "sub:%s:user:%s:token:term";
     // 终端下token信息
