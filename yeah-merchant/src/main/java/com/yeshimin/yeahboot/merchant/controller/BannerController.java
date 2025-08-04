ea67246eba34e29d5968acc2c6efed6b3b0b071a
yeshimin <ysm@yeshimin.com>
2025-08-04 22:37:13 +0800

    clean code

--- yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/BannerController.java
+++ yeah-merchant/src/main/java/com/yeshimin/yeahboot/merchant/controller/BannerController.java
@@ -34,7 +34,7 @@ public class BannerController extends ShopCrudController<BannerMapper, BannerEnt
     public BannerController(BannerRepo repo) {
         // 由于lombok方案无法实现构造方法中调用super，只能显式调用
         super(repo);
-        super.setModule("mch:banner");
+        super.setModule("mch:banner").disableCreate().disableUpdate().disableDelete();
     }
 
     // ================================================================================
