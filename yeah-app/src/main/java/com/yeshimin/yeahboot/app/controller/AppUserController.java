628b00eccb050b2fcb5da04fefb9c6b66bf17c15
yeshimin <ysm@yeshimin.com>
2025-08-12 20:49:14 +0800

    + springdoc

--- yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
+++ yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
@@ -5,6 +5,8 @@ import com.yeshimin.yeahboot.common.common.utils.WebContextUtils;
 import com.yeshimin.yeahboot.common.controller.base.BaseController;
 import com.yeshimin.yeahboot.common.domain.base.R;
 import com.yeshimin.yeahboot.data.domain.entity.AppUserEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * app端用户相关
  */
+@Tag(name = "app端用户相关")
 @Slf4j
 @RestController
 @RequestMapping("/app/appUser")
@@ -25,6 +28,7 @@ public class AppUserController extends BaseController {
     /**
      * 详情
      */
+    @Operation(summary = "详情")
     @GetMapping("/detail")
     public R<AppUserEntity> detail() {
         Long userId = WebContextUtils.getUserId();
