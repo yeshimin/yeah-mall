ce2d4ad1d6ed65c511217ea7f16babf1e061efea
yeshimin <ysm@yeshimin.com>
2025-08-12 20:56:33 +0800

    clean code

--- yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
+++ yeah-app/src/main/java/com/yeshimin/yeahboot/app/controller/AppUserController.java
@@ -6,8 +6,8 @@
 //import com.yeshimin.yeahboot.common.domain.base.R;
 //import com.yeshimin.yeahboot.data.domain.entity.AppUserEntity;
 //import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
