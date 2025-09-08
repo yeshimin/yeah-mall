19a488d75ba70d79769da3096fcfb5d1576bd938
yeshimin <ysm@yeshimin.com>
2025-09-08 23:54:36 +0800

    + ProductCollect

---
+++ yeah-biz-data/src/main/java/com/yeshimin/yeahboot/data/domain/entity/ProductCollectEntity.java
@@ -0,0 +1,25 @@
+package com.yeshimin.yeahboot.data.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.yeshimin.yeahboot.data.domain.base.ShopConditionBaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 商品收藏表
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("s_product_collect")
+public class ProductCollectEntity extends ShopConditionBaseEntity<ProductCollectEntity> {
+
+    /**
+     * 会员ID
+     */
+    private Long memberId;
+
+    /**
+     * 商品SPU ID
+     */
+    private Long spuId;
+}
