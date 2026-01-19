d6f72009faf9fce7287d636e3d00ded25df2f174
yeshimin <ysm@yeshimin.com>
2026-01-19 20:46:42 +0800

    module refactor

---
+++ yeah-biz-common/yeah-biz-data/src/main/java/com/yeshimin/yeahboot/data/domain/vo/AreaVo.java
@@ -0,0 +1,32 @@
+package com.yeshimin.yeahboot.data.domain.vo;
+
+import com.yeshimin.yeahboot.common.domain.base.BaseDomain;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class AreaVo extends BaseDomain {
+
+    /**
+     * 父编码
+     */
+    private String parentCode;
+
+    /**
+     * 编码
+     */
+    private String code;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 子集合
+     */
+    private List<AreaVo> children;
+}
